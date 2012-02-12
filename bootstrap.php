d5b857d1b278c13504272583dc1855d604dc953b
Benjamin Young <byoung@bigbluehat.com>
2012-02-12 10:21:17 -0500

    matching connect_with_flattr more closely

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,12 @@
 <?php
-require_once __DIR__ . '/lib/httpconnection.php';
-require_once __DIR__ . '/lib/httpresponse.php';
-require_once __DIR__ . '/lib/oauth2client.php';
+define('PROJECT_PATH', dirname(__FILE__));
+require PROJECT_PATH . '/coltrane/coltrane.php';
 
-if (is_file(__DIR__ . '/config.php')) {
-  require_once __DIR__ . '/lib/configbase.php';
-  require_once __DIR__ . '/config.php';
-}
-// simple log wraps error_log, 
+// require oauth2client, httpconnection, httpresponse and configbase
+require_libs(__DIR__.'/../lib');
+// include the api config
+require __DIR__.'/../config.php';
+
 function slog($message)
 {
   if (is_array($message)) {
@@ -24,13 +23,3 @@ function slog($message)
     error_log($message."\n", 3, ConfigFlattr::$LOGFILE);
   }
 }	  
-
-function h($str)
-{
-  return htmlentities($str,ENT_QUOTES, "UTF-8");
-}
-
-if (php_sapi_name() != 'cgi' && !empty($_SERVER['REMOTE_ADDR'])) {
-  session_start();
-  header('Content-type: text/html; charset=utf-8');
-}
