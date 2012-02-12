53527ecc4e58389fe8500355b6ff7bf9646dfff1
Benjamin Young <byoung@bigbluehat.com>
2012-02-12 11:47:22 -0500

    WIP: starting CouchDB integration

--- public/authenticated.php
+++ public/authenticated.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . '/../bootstrap.php';
+require __DIR__ . '/../lib/sag/Sag.php';
 
 function on_before($params)
 {
@@ -19,6 +20,20 @@ function on_get($params)
     'things'  => $params['client']->getParsed('/user/things'),
     'title'   => 'signed in!',
   );
+
+  $sag = new Sag('bigbluehat.ic.tl', '5984');
+  $sag->setDatabase('phlattr');
+  print_r($sag->get($_SESSION['flattr_username']));
+  // get list of existing registered phones and display them
+  // if it 404's, create it so we'll have it later
   return template(basename(__FILE__),$vars);
+}
+
+function on_post($params) {
+  // get the list of phones
+  // add the new submitted phone to the list
+  // load the GET resource
+  $json = array('user' => $_SESSION['flattr_username'],
+                'phones' => array());
 }
 run(basename(__FILE__),'.php');
